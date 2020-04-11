64e4bb31553c624228fecf283caf79e0da3844ad
Maxim Vanyushkin <max.vanyushkin@gmail.com>
2020-04-11 16:43:17 +0400

    HW - added goose to the external deps list

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,6 +1,7 @@
 echo "-> Loading modules..."
 go get -u github.com/golang/protobuf/protoc-gen-go
 go get -u golang.org/x/lint/golint
+go get -u github.com/pressly/goose/cmd/goose
 go mod download
 echo "[x] Done."
 echo "-> Generating grpc contracts..."
